1c05e8640f2a57073d7d8adbc961c86d715e1b4c
Duarte Cancela <duartecancela@gmail.com>
2021-11-24 15:54:22 +0000

    implement remember me token

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,8 @@
                 <div class="mb-4">
                     <label for="email" class="sr-only">Email</label>
                     <input type="text" name="email" id="email" placeholder="Your email"
-                           class="bg-gray-100 border-2 w-full p-4 rouded-lg  @error('name') border-red-500 @enderror" value="">
+                           class="bg-gray-100 border-2 w-full p-4 rouded-lg  @error('name') border-red-500 @enderror"
+                           value="">
                     @error('email')
                     <div class="text-red-500 mt-2 text-sm">
                         {{ $message}}
@@ -23,15 +24,24 @@
                 <div class="mb-4">
                     <label for="password" class="sr-only">Password</label>
                     <input type="password" name="password" id="password" placeholder="Choose password"
-                           class="bg-gray-100 border-2 w-full p-4 rouded-lg  @error('name') border-red-500 @enderror" value="">
+                           class="bg-gray-100 border-2 w-full p-4 rouded-lg  @error('name') border-red-500 @enderror"
+                           value="">
                     @error('password')
                     <div class="text-red-500 mt-2 text-sm">
                         {{ $message}}
                     </div>
                     @enderror
+                </div>
+                <div class="mb-4">
+                    <div>
+                        <input type="checkbox" name="remember" id="remember" class="mr-2">
+                        <label for="remember">Remember me</label>
+                    </div>
+                </div>
                 <div>
                     <button type="submit" class="bg-blue-500 text-white px-4 py-3 rouded font-medium w-full">
-                        Login</button>
+                        Login
+                    </button>
                 </div>
             </form>
         </div>
